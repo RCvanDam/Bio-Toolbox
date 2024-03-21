5d184c08cbac7855847125fd958ddb935f94a2a3
Fl-ris <florismenninga@gmail.com>
2024-03-21 21:17:46 +0100

    Inproved FIMO class

--- FIMO_MEME_Commandline.py
+++ FIMO_MEME_Commandline.py
@@ -3,7 +3,7 @@
 MemeSuite backend for website
 Author: Floris M
 Date: 7-03-2024
-Last updated: 18-03-2024
+Last updated: 21-03-2024
 
 Version: 0.05
 
@@ -27,6 +27,16 @@ class Fimo:
 
     def __str__(self):
         return f"Database used: {database_to_use}, Default p-value used?: {use_default_p_value}, P-value: {p_value}"
+    
+    def run(self):
+        if database_to_use: # If the user selected one of the databases instead of a motif file. 
+            pass
+        else: # if not: use a given motif file.
+            fimo_command = "fimo --oc {output_path_fimo} --verbosity 1 --bgfile --nrdb-- --thresh {p_value} {input_motif_file} {input_sequence_path_fimo}".format()
+
+        fimo_output = subprocess.run([fimo_command], shell=True)
+        output_fimo = fimo_output.stdout
+        print(output_fimo) # should be redirected to the ouput display in the website. 
 
 
 class Meme:
@@ -41,9 +51,11 @@ class Meme:
     def __str__(self):
         return f"Max amount of motifs: {self.max_amount_of_motifs}, Max motif size: {self.max_motif_size}, Min motif size: {self.min_motif_size}, Alphabet used: {self.alphabet}."
 
-    def run(self): # add commandline execution here...
+
+    def run(self): # add commandline execution using the user given parameters.
       #  meme_command_test = "meme '/home/floris/Documenten/Data_set/DATA/meme_sample_sequences' -dna -oc ~/Documenten/OUTPUT_DATA/MEME/ -time 14400 -mod zoops -nmotifs 3 -minw 6 -maxw 50 -objfun classic -revcomp -markov_order 0"
-        meme_command_test = "meme '/home/floris/Documenten/Data_set/DATA/meme_sample_sequences' -dna -oc ~/Documenten/OUTPUT_DATA/MEME/ -time 14400 -mod zoops -nmotifs 3 -minw 6 -maxw 50 -objfun classic -revcomp -markov_order 0".format()
+        
+        meme_command_test = "meme {input_sequence_path_meme} {alphabet} -oc {output_path_meme} -time 14400 -mod zoops {max_amount_of_motifs} -minw 6 -maxw 50 -objfun classic -revcomp -markov_order 0".format()
         meme_output = subprocess.run([meme_command_test], shell=True)
         output_meme = meme_output.stdout
         print(output_meme) # should be redirected to the ouput display in the website. 
@@ -69,6 +81,8 @@ def receive_input():
     min_motif_size = 0 
     alphabet = "DNA" # Nucleotide alphabet to use: RNA, DNA or protein.
 
+
+    # Running the tools using the classes.
     meme_test = Meme(max_amount_of_motifs, max_motif_size, min_motif_size, alphabet) # Make Meme instance
     print(str(meme_test)) # print information about the meme_test object.
     meme_test.run()
@@ -101,4 +115,3 @@ if __name__ == "__main__":
     database_to_use, use_default_p_value, p_value, max_amount_of_motifs, max_motif_size, min_motif_size, alphabet = receive_input()
     fimo_command, meme_command = input_commands()
   #  process_commands(fimo_command, meme_command)
-
