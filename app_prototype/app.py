91d45eedbb8ca63f97fcf4c140be654b937bd5a9
Mooncake98 <t.t.vo@st.hanze.nl>
2024-03-27 15:27:02 +0100

    removed other variable from fimo backend

--- app_prototype/app.py
+++ app_prototype/app.py
@@ -100,7 +100,6 @@ def html_render_fimo():
     elif method == "POST":#user submitted inputs
         user_input_values = { #here I save all the input button values as variables
         "chosen_database": request.form["chosen_database"],
-        "other_variable": request.form["other_variable"],
         "input_custom_pvalue": request.form["input_custom_pvalue"],
         "max_amount_motifs": request.form["max_amount_motifs"],
         "maxsize": request.form["maxsize"],
@@ -176,9 +175,6 @@ def html_render_meme():
     elif request.method == "POST":#user submitted inputs
             user_input_values = { #here I save all the input button values as variables
             "max_motif_amount": request.form["max_motif_amount"],
-            "other_variable1": request.form["other_variable1"],
-            "other_variable2": request.form["other_variable2"],
-            "other_variable3": request.form["other_variable3"],
             "max_motif_size": request.form["max_motif_size"],
             "min_motif_size": request.form["min_motif_size"],
             } # request.form refers to the inputlabel's name="" in html page
@@ -197,7 +193,7 @@ def html_render_meme():
             output_path_meme = "{}/User_ouput/meme".format(working_dir)
 
             # execute Meme with user parameters
-            meme = Meme(user_input_values["max_amount_of_motifs"], user_input_values["max_motif_size"], user_input_values["min_motif_size"], "dna", input_sequence_path_meme, output_path_meme)
+            meme = Meme(user_input_values["max_motif_amount"], user_input_values["max_motif_size"], user_input_values["min_motif_size"], "dna", input_sequence_path_meme, output_path_meme)
             print(str(meme)) # redirect to website
             meme.run()
 
