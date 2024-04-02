1ade7ece14f254faea2c8209e314f1a536ee3d6a
RCvanDam <r.vandam1000@gmail.com>
2024-04-02 15:08:04 +0200

    commented shutil

--- app_prototype/FIMO_MEME_Commandline.py
+++ app_prototype/FIMO_MEME_Commandline.py
@@ -25,13 +25,13 @@ use_default_p_value = True # True or false
 p_value = 0.0001 # float, default is 0.0001.
 input_motif_file = "{}/Motif_databases/SwissRegulon_e_coli.meme".format(working_dir)
 input_sequence_path_fimo = "{}/meme_sample_sequences.fasta".format(working_dir) # replace with relative path.
-output_path_fimo = "{}/User_output/fimo".format(working_dir) # (Temporary) storage place for the generated files. 
+output_path_fimo = "{}/User_output/fimo".format(working_dir) # (Temporary) storage place for the generated files.
 
 
 # Test variables for MEME
 max_amount_of_motifs = 20 # max abount of motif to look for, program stops looking if the number is exceeded.
 max_motif_size = 8 # max length of the motifs.
-min_motif_size = 3 
+min_motif_size = 3
 alphabet = "dna" # Nucleotide alphabet to use: RNA, DNA or protein.
 input_sequence_path_meme = "{}/meme_sample_sequences.fasta".format(working_dir)
 output_path_meme = "{}/User_output/meme".format(working_dir)
@@ -44,7 +44,7 @@ class Fimo:
 
     :param database_to_use: The motif file to use when the user doesn't provide their own.
     :param use_default_p_value: True or False, whether or not to use the default P-value.
-    :param p_value: If chosen, the custom P-value. 
+    :param p_value: If chosen, the custom P-value.
     """
 
     def __init__(self, database_to_use, use_default_p_value, p_value, input_motif_file, input_sequence_path_fimo, output_path_fimo):
@@ -58,7 +58,7 @@ class Fimo:
 
     def __str__(self):
         """
-        Returns information about the created object. 
+        Returns information about the created object.
         """
         return f"Database used: {database_to_use}, Default p-value used?: {use_default_p_value}, P-value: {p_value}"
 
@@ -82,18 +82,18 @@ class Fimo:
 
     def run(self):
         """
-        Using the ".run()" method, the Fimo tool will be run. No parameters needed because they were already given when the class was initialized. 
+        Using the ".run()" method, the Fimo tool will be run. No parameters needed because they were already given when the class was initialized.
         """
 
 
-        if database_to_use: # If the user selected one of the databases instead of a motif file. 
+        if database_to_use: # If the user selected one of the databases instead of a motif file.
             pass # add logic to select database depending on the user selection.
         else: # if not: use a given motif file.
             fimo_command = "fimo --oc {} --verbosity 2 --bgfile --nrdb-- --thresh {} {} {}".format(output_path_fimo, p_value, input_motif_file, input_sequence_path_fimo)
 
         fimo_output = subprocess.run([fimo_command], executable="/bin/sh", shell=True, text=True)
         output_fimo = fimo_output.stdout
-        print(output_fimo) # should be redirected to the ouput display in the website. 
+        print(output_fimo) # should be redirected to the ouput display in the website.
 
 
 class Meme:
@@ -102,7 +102,7 @@ class Meme:
     :param: # to-do fill in params
 
     """
-        
+
     def __init__(self, max_amount_of_motifs, max_motif_size, min_motif_size, alphabet, input_sequence_path_meme, output_path_meme):
         self.max_amount_of_motifs = max_amount_of_motifs
         self.max_motif_size = max_motif_size
@@ -111,13 +111,13 @@ class Meme:
         self.input_sequence_path_meme = input_sequence_path_meme
         self.output_path_meme = output_path_meme
         self.new_env = None
-    
+
     def __str__(self):
         return f"Max amount of motifs: {self.max_amount_of_motifs}, Max motif size: {self.max_motif_size}, Min motif size: {self.min_motif_size}, Alphabet used: {self.alphabet}."
 
     def add_to_path(self):
         """
-        If memeSuite is not added to the PATH, this method can add it. 
+        If memeSuite is not added to the PATH, this method can add it.
 
         """
         print("in Path?")
@@ -179,16 +179,16 @@ class Meme:
         for filename in files:
             file.add(f"{output_path_meme}/{filename}", filename)
 
-        
+
         # shutil.rmtree(f"{output_path_meme}", ignore_errors=True)
-        
+
 def extension_check(fastafile):
     """made this path checker again re test if it works
     :param: fastafile
     :return: True or false"""
     if os.path.splitext(fastafile)[1] == ".fasta":
         return True
-        
+
 def is_multifasta(fastafile: str):
     """
     Detects whether a file is fasta or multifasta
@@ -219,12 +219,12 @@ def receive_input():
     meme_test.run()
     print(str(meme_test)) # print information about the meme_test object.
 
-    #meme_test.add_to_path()
-
+    # meme_test.add_to_path()
+    #
     # Fimo test:
     # fimo_test = Fimo(database_to_use, use_default_p_value, p_value, input_motif_file, input_sequence_path_fimo, output_path_fimo)
     # fimo_test = Fimo(database_to_use, use_default_p_value, p_value, input_motif_file, input_sequence_path_fimo, output_path_fimo)
-
+    #
     # fimo_test.run() # execute the commandline tool using the object fimo_test.
     # print(str(fimo_test)) # print information from the def __str__ function in the Meme class.
 
@@ -232,7 +232,7 @@ def receive_input():
 
 
 def input_commands():
-    
+
     fimo_command = "fimo --oc <OUTPUT_LOCATION> --verbosity 1 --bgfile --nrdb-- --thresh 1.0E-4 <Motif_file_location> <Fasta_file_location>"
     meme_command = "meme '/home/floris/OneDrive_FLORIS/Hanze_Bioinformatica/Jaar_1/Projecten/PAM_Scoring_matrix/DATA/meme_sample_sequences' -dna -oc ~/Documenten/OUTPUT_DATA/MEME/ -time 14400 -mod zoops -nmotifs 3 -minw 6 -maxw 50 -objfun classic -revcomp -markov_order 0"
 
@@ -255,4 +255,4 @@ if __name__ == "__main__":
     database_to_use, use_default_p_value, p_value, max_amount_of_motifs, max_motif_size, min_motif_size, alphabet = receive_input()
     fimo_command, meme_command = input_commands()
     #process_commands(fimo_command, meme_command)
-    
+
