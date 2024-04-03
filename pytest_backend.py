ea707fbec20ba8299ab9f901a0259602fd044ef1
Fl-ris <florismenninga@gmail.com>
2024-04-03 20:58:03 +0200

    added initial pytest frame

---
+++ pytest_backend.py
@@ -0,0 +1,19 @@
+"""
+Python unit-test for the Homepage, Memepage and Fimopage. 
+Date: 25-03-2024
+Author: Floris M
+
+"""
+
+import unittest
+
+from FIMO_MEME_Commandline import Fimo, Meme
+
+
+
+def test_fimo_backend():
+    Fimo
+    
+
+
+
