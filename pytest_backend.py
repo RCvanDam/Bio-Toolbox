400af9321e567a5acb30831892c046d18cfdfa56
Fl-ris <florismenninga@gmail.com>
2024-04-04 09:59:12 +0200

    added initial pytest frame for more functions

--- pytest_backend.py
+++ pytest_backend.py
@@ -4,18 +4,28 @@ Date: 25-03-2024
 Author: Floris M
 
 """
+import os
+working_dir = os.path.dirname(os.path.realpath(__file__)) # to check current dir
 
 import unittest
 
-from FIMO_MEME_Commandline import Fimo, Meme
+
+path = "app_prototype/FIMO_MEME_Commandline"
+tool = "{}/{}".format(working_dir, path)
+import "{}/{}".format(working_dir, path)
 
 
 
 def test_fimo_backend():
+    pass
 
 
 def test_meme_backend():
-    
+    pass
+
+def test_multi_fasta(fasta, multifasta):
+    is_multifasta(fasta, multifasta)
+
 
     
 
